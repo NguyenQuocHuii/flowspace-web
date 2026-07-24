9a95e85630dd8972e46cc4cea501387b11c079cd
NguyenQuocHuii <nguyenquochuy.nqh05@gmail.com>
2026-07-24 18:18:04 +0700

    fix(be): clear test file in application layer for clean production deployment

--- be/src/FlowSpace.Application/Services/GanttCycleDetectionTests.cs
+++ be/src/FlowSpace.Application/Services/GanttCycleDetectionTests.cs
@@ -1,14 +1,1 @@
-using System;
-using System.Collections.Generic;
-using FlowSpace.Domain.Entities;
-
-namespace FlowSpace.Application.Services
-{
-    public static class GanttCycleValidator
-    {
-        public static bool ValidateCycle(Guid source, Guid target, List<TaskDependency> links)
-        {
-            return GanttService.HasCycle(source, target, links);
-        }
-    }
-}
+// FlowSpace Application - Gantt Service Helper
